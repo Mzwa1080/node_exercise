a7e4d20b6abf96244d40d382842aaf084a68c94f
Mzwa1080 <mshawnlouw@gmail.com>
2024-02-07 12:12:30 +0200

    yoowgit add .

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var fs = require('fs');
+// var fs = require('fs');
 
-fs.writeFile('multiplication.js', 'You can create the functiob below',function (err) {
-  if (err) throw err;
-  console.log('Saved!');
-});
+// fs.writeFile('multiplication.js', 'You can create the functiob below',function (err) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
 
-fs.writeFile('division.js', 'i believe in you',function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });
+// fs.writeFile('division.js', 'i believe in you',function (err) {
+//     if (err) throw err;
+//     console.log('Saved!');
+//   });
 
 
-let {multiplication} = require('./multiplication')
+let {mutlitply} = require('./multiplication')
 let {division} = require('./division')
 
-console.log(multiplication(2,3));
+console.log(mutlitply(2,3));
 console.log(division(2,1));
