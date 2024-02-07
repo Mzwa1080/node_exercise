35c18768da2589152c70f73903da1a2448d4643b
Mzwa1080 <mshawnlouw@gmail.com>
2024-02-07 14:02:45 +0200

    added few lines of code

--- index.js
+++ index.js
@@ -10,9 +10,20 @@
 //     console.log('Saved!');
 //   });
 
-
 let {mutlitply} = require('./multiplication')
 let {division} = require('./division')
 
 console.log(mutlitply(2,3));
 console.log(division(2,1));
+
+// ___________________________________
+
+const http = require('http');
+const port = +process.env.port || 4000;
+
+http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<center><h1>Welcome Home To Our Page!</h1> <h3> This font is in h3 bih!!</h3>\n </center>');
+}).listen(port , ()=> {
+  console.log(`server is running on http://localhost:${port}`);
+});
